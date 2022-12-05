98ce3e2312ca793dc2c37c8d7dcb6ca6fd72a2a0
sathishkd1827 <sathishkongu6@gmail.com>
2022-12-05 18:25:48 +0530

    Code

---
+++ src/test/java/org/git/ClassA.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class ClassA 
+{
+	
+	private void tc1() 
+	{
+		System.out.println("First");
+	}
+	
+	private void tc2() 
+	{
+		System.out.println("Second");
+	}
+	
+	private void tc3() 
+	{
+		System.out.println("Third");
+	}
+}
